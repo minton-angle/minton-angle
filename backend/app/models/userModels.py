53f1ae4ce1c5af42603bb26994312323c33ff9ca
jueun27 <eunn1027@gmail.com>
2026-02-06 11:52:17 +0900

    [Feat]postgreSQL연동

--- backend/app/models/userModels.py
+++ backend/app/models/userModels.py
@@ -1,1 +1,21 @@
-# 사용자 정보 (ID,PW,닉네임)
+"""
+USER 모델
+"""
+
+from sqlalchemy import Column, String, Text, Date
+from sqlalchemy.sql import func
+
+from app.db.base import Base
+
+
+class User(Base):
+    """사용자"""
+    __tablename__ = "user"
+    
+    id = Column(String(16), primary_key=True)  # 사용자 ID (PK)
+    name = Column(String(16), nullable=False)  # 사용자 이름
+    password = Column(Text, nullable=False)  # 비밀번호
+    create_date = Column(Date, server_default=func.now())  # 가입일
+    
+    def __repr__(self):
+        return f"<User {self.id} - {self.name}>"
