084eaa07cc63455316d11981768d2ad5390bcf39
mjmjkkk <kmjsophia1@naver.com>
2026-02-24 09:46:20 +0900

    [Feat] html 수정

--- backend/app/db/base.py
+++
@@ -1,8 +0,0 @@
-# 모델 통합 관리 (Base)
-"""
-SQLAlchemy Base 클래스
-"""
-
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
--- backend/app/services/swing/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .swing_service import swing_service
-# from .video_analysis_service import video_analysis_service
-
-# __all__ = [
-#     'swing_service',
-#     'video_analysis_service'
-# ]
